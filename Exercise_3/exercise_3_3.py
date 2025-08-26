b7d8137cbf21c0094e28d5d83f283e088626a387
Samnap Peo <Samnappeo@gmail.com>
2025-08-26 13:10:54 +0300

    Made changes to exercise_3_3. Removed intersex to better align with task given.

--- Exercise_3/exercise_3_3.py
+++ Exercise_3/exercise_3_3.py
@@ -1,14 +1,9 @@
-sex = input("""Select your biological sex: 
+while True:
+    sex = input("""Select your biological sex: 
 A. Male 
 B. Female
-C. Intersex
 Enter selection: """)
-
-while True:
-    if sex == "C".casefold():
-        print("Sorry, we do not have enough data to gauge if your hemoglobin values are within normal range.")
-        break
-    elif sex == "A".casefold():
+    if sex == "A".casefold() or sex == "MALE".casefold():
         hg_lvl = float(input("Please enter your hemoglobin level: "))
         if  134 <= hg_lvl <= 167:
             print("Your hemoglobin levels are within normal range")
@@ -19,7 +14,7 @@ while True:
         else:
             print("Your hemoglobin levels are too low")
             break
-    elif sex == "B".casefold():
+    elif sex == "B".casefold() or sex == "FEMALE".casefold():
         hg_lvl = float(input("Please enter your hemoglobin level: "))
         if  117 <= hg_lvl <= 155:
             print("Your hemoglobin levels are within normal range")
@@ -32,5 +27,3 @@ while True:
             break
     else:
         print("Error. Please try again.")
-        sex = "not selected"
-        break
