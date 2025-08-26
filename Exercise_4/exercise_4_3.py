5855f24b2ec445c9d6ca3e1d7161d40e02715868
Samnap Peo <Samnappeo@gmail.com>
2025-08-26 16:50:12 +0300

    In exercise_4_3 "horse_shoe" was changed to "None" and a condition for when the user enters no numbers is created. Also, I learned that
    if x: works the same as if != []

--- Exercise_4/exercise_4_3.py
+++ Exercise_4/exercise_4_3.py
@@ -1,4 +1,4 @@
-input_value = "horse_shoe"
+input_value = None
 min_max_list = []
 print(f"Enter a bunch of numbers, we'll let you know which is the "
       f"highest and lowest.")
@@ -8,8 +8,10 @@ while input_value != "":
         input_value = int(input_value)
         min_max_list.append(input_value)
 
-
-min_value = min(min_max_list)
-max_value = max(min_max_list)
-print(f"The lowest number you entered was {min_value}, and the highest "
+if min_max_list:
+    min_value = min(min_max_list)
+    max_value = max(min_max_list)
+    print(f"The lowest number you entered was {min_value}, and the highest "
       f"was {max_value}.")
+
+print("You didnt enter any numbers.")
